a550098b04584360aa596a39649814dd780f5e57
Terra Roush <terra.roush@gmail.com>
2021-03-12 15:39:32 -0600

    setup data and images

---
+++ src/components/MatchingGame.vue
@@ -0,0 +1,22 @@
+<template>
+  
+</template>
+
+<script>
+import getTripPicsCards from "../data";
+import shuffle from "lodash.shuffle";
+
+export default {
+data() {
+    return {
+        tripPics: shuffle(getTripPicsCards()),
+        guess1: null,
+        guess2: null,
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/data.js
@@ -0,0 +1,148 @@
+export default function getTripPicsCards() {
+    return [
+      {
+        id: 1,
+        name: "gasoline",
+        image: "gasoline.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 2,
+        name: "gasoline",
+        image: "gasoline.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 3,
+        name: "fishBalls",
+        image: "fishBalls.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 4,
+        name: "fishBalls",
+        image: "fishBalls.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 5,
+        name: "lanterns",
+        image: "lanterns.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 6,
+        name: "lanterns",
+        image: "lanterns.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 7,
+        name: "morningMarket",
+        image: "morningMarket.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 8,
+        name: "morningMarket",
+        image: "morningMarket.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 9,
+        name: "natureWins",
+        image: "natureWins.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 10,
+        name: "natureWins",
+        image: "natureWins.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 11,
+        name: "scottCooks",
+        image: "scottCooks.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 12,
+        name: "scottCooks",
+        image: "scottCooks.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 13,
+        name: "sunset",
+        image: "sunset.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 14,
+        name: "sunset",
+        image: "sunset.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 15,
+        name: "terraCooks",
+        image: "terraCooks.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 16,
+        name: "terraCooks",
+        image: "terraCooks.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 17,
+        name: "noodles",
+        image: "noodles.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 18,
+        name: "noodles",
+        image: "noodles.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 19,
+        name: "wrappedTemple",
+        image: "wrappedTemple.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 20,
+        name: "wrappedTemple",
+        image: "wrappedTemple.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 21,
+        name: "coldSoup",
+        image: "coldSoup.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 22,
+        name: "coldSoup",
+        image: "coldSoup.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 23,
+        name: "festivalSelfie",
+        image: "festivalSelfie.jpeg",
+        foundMatch: false
+      },
+      {
+        id: 24,
+        name: "festivalSelfie",
+        image: "festivalSelfie.jpeg",
+        foundMatch: false
+      }
+    ];
+  }
